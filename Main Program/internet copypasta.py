c709605b69f79b92bfa26378c489ceebfeaeb56b
Humza Anwar <humza.anwar2330@gmail.com>
2019-08-17 09:53:50 -0400

    haha yes

--- Main Program/internet copypasta.py
+++
@@ -1,5 +0,0 @@
-import os
-for root, dirs, files in os.walk("/home/humza/Music/DCIM"):
-    for file in files:
-        if file.endswith(".mp3"):
-             print(os.path.join(root, file))
--- Recursion/Palindromes/iterative_palindromes.py
+++
@@ -1,6 +0,0 @@
-word = input("what word\n")
-while(len(word) > 1): 
-	if(word[0] == word[-1]): 
-		word = word[1:-2]
-	else: 
-		print ("False")
